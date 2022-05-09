e43f3be03be288b23a0c54e1ab57effbcd2b5f5f
Carlos Flores <carlosfloresalex@gmail.com>
2022-05-08 19:44:14 -0700

    edit href for footer links

--- src/pages/Components/Footer/index.js
+++ src/pages/Components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
   return (
@@ -7,34 +7,64 @@ function Footer() {
         <div className="container">
           <div className="row mb-5">
             <div className="col-md-3">
-              <h3 className="footer-title"><b><a href="/">P'Unity</a></b></h3>
-              <p>An unique new type of a Online Community around Computer Science. </p>
-              <p><span className="d-inline-block d-md-block">Developed specifically for performance, live comments, an intelligent code engine and more.</span></p>
+              <h3 className="footer-title">
+                <b>
+                  <a href="/">P'Unity</a>
+                </b>
+              </h3>
+              <p>
+                An unique new type of a Online Community around Computer
+                Science.{' '}
+              </p>
+              <p>
+                <span className="d-inline-block d-md-block">
+                  Developed specifically for performance, live comments, an
+                  intelligent code engine and more.
+                </span>
+              </p>
             </div>
             <div className="col-md-5 mx-auto">
               <div className="row">
                 <div className="col-lg-4">
                   <h3 className="footer-title">Product</h3>
                   <ul className="list-unstyled">
-                    <li><a href="/#features">Features</a></li>
-                    <li><a href="/#pricing">Pricing</a></li>
-                    <li><a href="#">More</a></li>
+                    <li>
+                      <a href="/#features">Features</a>
+                    </li>
+                    <li>
+                      <a href="/#pricing">Pricing</a>
+                    </li>
+                    <li>
+                      <a href="/#">More</a>
+                    </li>
                   </ul>
                 </div>
                 <div className="col-lg-4">
                   <h3 className="footer-title">Resources</h3>
                   <ul className="list-unstyled">
-                    <li><a href="#">More</a></li>
-                    <li><a href="#">More</a></li>
-                    <li><a href="#">More</a></li>
+                    <li>
+                      <a href="/#">More</a>
+                    </li>
+                    <li>
+                      <a href="/#">More</a>
+                    </li>
+                    <li>
+                      <a href="/#">More</a>
+                    </li>
                   </ul>
                 </div>
                 <div className="col-lg-4">
                   <h3 className="footer-title">Feedback</h3>
                   <ul className="list-unstyled">
-                    <li><a href="#">More</a></li>
-                    <li><a href="#">More</a></li>
-                    <li><a href="#">More</a></li>
+                    <li>
+                      <a href="/#">More</a>
+                    </li>
+                    <li>
+                      <a href="/#">More</a>
+                    </li>
+                    <li>
+                      <a href="/#">More</a>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -42,10 +72,16 @@ function Footer() {
             <div className="col-md-3">
               <h3 className="footer-title">Contact</h3>
               <ul className="list-unstyled">
-                    <li><a href="#">More</a></li>
-                    <li><a href="#">More</a></li>
-                    <li><a href="#">More</a></li>
-                  </ul>
+                <li>
+                  <a href="/#">More</a>
+                </li>
+                <li>
+                  <a href="/#">More</a>
+                </li>
+                <li>
+                  <a href="/#">More</a>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="row">
